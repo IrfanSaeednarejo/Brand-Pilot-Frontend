c1275006c0e51111a8fafed94061bdd84d0bd4b3
IrfanSaeednarejo <irfansaeed.narejo@gmail.com>
2025-07-02 18:54:59 +0500

    initial edit for connection

--- src/router.jsx
+++ src/router.jsx
@@ -5,13 +5,13 @@ import AboutUs from "./Pages/AboutUS";
 import ContactUs from "./Pages/ContactUS";
 import Login from "./Components/Login";
 import Signup from "./Components/SignUP";
-import UserProfilePage from "./Pages/UserProfile";
+
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 
 import ContentGenerator from './Pages/ContentGenerator'
 import OutputPreview from './Pages/OutputPreview'
-import ContentHistory from './Pages/ContentHistory'
+
 
 const AppRouter = () => {
   return (
@@ -24,10 +24,10 @@ const AppRouter = () => {
           <Route path='/contact' element={<ContactUs />} />
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
-          {/* <Route path='/profile' element={<UserProfilePage />} /> */}
+         
           <Route path='/generate' element={<ContentGenerator />} />
           <Route path='/output' element={<OutputPreview />} />
-          {/* <Route path='/history' element={<ContentHistory />} /> */}
+        
 
         </Routes>
         <Footer />
